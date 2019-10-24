cd17b78bd4f4808e119f9cb66b73da157ea8724c
Piotr Rachwalik <piotr.rachwalik@gmail.com>
2019-10-24 09:06:09 +0200

    make output more fancy

--- src/main/java/pl/krakow/uek/pira/ppv/creditcard/App.java
+++ src/main/java/pl/krakow/uek/pira/ppv/creditcard/App.java
@@ -2,6 +2,6 @@ package pl.krakow.uek.pira.ppv.creditcard;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world :_+");
     }
 }
